685f2c9fbc2b284036eba8b7f323106b10b8719f
thiago lopes de mello <timabuntu@gmail.com>
2022-08-22 11:11:05 -0300

    feat: escape enter

--- fundamentals/hello-world.sh
+++ fundamentals/hello-world.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 
-#? Todas essas execuções são feitas no Shell filho
+#? Escapando o Enter
 
-echo "Aonde eu estou?"
-cd /
-pwd
-
-echo "Aonde eu estou?"
-cd /home
-pwd
-
-echo "Aonde eu estou?"
-cd /home/fast
-pwd
-
-echo "Aonde eu estou?"
-cd /home/fast/timabuntu
-pwd
+echo \
+  "Meu amigo, tudo bem com você?"
